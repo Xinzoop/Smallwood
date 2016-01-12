6ec73f074d2176a6b4f50fe8cd29ff06ee21880e
Xinzoop <xinzoop@gmail.com>
2016-01-12 18:09:55 +1100

    .NET MVC

---
+++ ASS3/Controllers/StudentsController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ASS3.Models;
+using System.Data.Entity.Validation;
+using System.Diagnostics;
+using PagedList;
+
+namespace ASS3.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private ASS3Entities1 db = new ASS3Entities1();
+
+        // GET: Students
+        public ActionResult Index()
+        {
+            var students = db.Students.Include(s => s.Course);
+            // Student sort by first name
+            return View(students.OrderBy(s => s.fname).ToPagedList(1, 5));
+        }
+
+        public PartialViewResult Search(String term, int page)
+        {
+            var students = db.Students.Include(s => s.Course);
+
+            if (String.IsNullOrEmpty(term))
+                // Student sort by first name
+                return PartialView(students.OrderBy(s => s.fname).ToPagedList(page, 5));
+            return PartialView(students.Where(c => c.fname.Contains(term) || c.lname.Contains(term)).OrderBy(s => s.fname).ToPagedList(page, 5));
+        }
+
+        // GET: Students/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = db.Students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        // GET: Students/Create
+        public ActionResult Create()
+        {
+            ViewBag.course_id = new SelectList(db.Courses, "Id", "Code");
+            return View();
+        }
+
+        // POST: Students/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,fname,lname,course_id,enrol_date")] Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Students.Add(student);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.course_id = new SelectList(db.Courses, "Id", "Code", student.course_id);
+            return View(student);
+        }
+
+        // GET: Students/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = db.Students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.course_id = new SelectList(db.Courses, "Id", "Code", student.course_id);
+            return View(student);
+        }
+
+        // POST: Students/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,fname,lname,course_id,enrol_date")] Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    db.Entry(student).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                catch (DbEntityValidationException dbEx)
+                {
+                    foreach (var validationErrors in dbEx.EntityValidationErrors)
+                    {
+                        foreach (var validationError in validationErrors.ValidationErrors)
+                        {
+                            Trace.TraceInformation("Property: {0} Error: {1}",
+                                validationError.PropertyName, validationError.ErrorMessage);
+                        }
+                    }
+                }
+            }
+            ViewBag.course_id = new SelectList(db.Courses, "Id", "Code", student.course_id);
+            return View(student);
+        }
+
+        // GET: Students/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = db.Students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        // POST: Students/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Student student = db.Students.Find(id);
+            db.Students.Remove(student);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
